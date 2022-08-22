aaed583c8fa071f62d0111f69cb4e32d61ea7ffc
Utkarsh00047 <utkarsh.gupta@bhavnacorp.com>
2022-08-22 17:00:30 +0530

    component

--- Angular/22-Aug/first-project/src/app/app.module.ts
+++ Angular/22-Aug/first-project/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstCompComponent } from './first-comp/first-comp.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstCompComponent
+    FirstCompComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
