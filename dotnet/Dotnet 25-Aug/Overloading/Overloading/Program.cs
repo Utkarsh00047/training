4978ba0cbf345814c645f5a0fb61292622dc7fbc
Utkarsh00047 <utkarsh.gupta@bhavnacorp.com>
2022-08-25 13:03:49 +0530

    overrloading

---
+++ dotnet/Dotnet 25-Aug/Overloading/Overloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Overloading
+{
+    public class overloading    
+    {
+        public static int add2(int one, int two)
+        {
+            Console.Write("sum of two numbers ");
+            return one + two;
+        }
+        public static int add3(int one, int two, int three)
+        {
+            Console.WriteLine("sum of 3 ");
+            return one + two + three;
+        }
+        public static string Numst_ring(string one, string two, string three, string four)
+        {
+            Console.WriteLine("concatenated string");
+            return one + two + three + four;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(overloading.add2(2,3));
+            Console.WriteLine(overloading.add3(1, 2, 3));
+            Console.WriteLine(overloading.Numst_ring("1", "2", "3", "4"));
+        }
+    }
+}
