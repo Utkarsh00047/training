803f3d11e568fd3c05b09354499898d6c492d8f4
Utkarsh00047 <utkarsh.gupta@bhavnacorp.com>
2022-08-30 18:06:31 +0530

    trigger

--- dotnet/30-Aug/banking/Banking_db.sql
+++ dotnet/30-Aug/banking/Banking_db.sql
@@ -1,3 +1,4 @@
+use banking
 CREATE TABLE admin_details
 	(
 		id INT PRIMARY KEY,
@@ -12,13 +13,35 @@ CREATE TABLE admin_details
 
 CREATE TABLE cust_details
 	(
-		id INT IDENTITY(1,1) PRIMARY KEY,
+		userid INT IDENTITY(1,1) PRIMARY KEY,
         cust_name VARCHAR(25),
 		cust_email VARCHAR(25),
 		cust_acc_no VARCHAR(25),
 		balance VARCHAR(25),
     );
+	drop table cust_details
 	
 	select * from cust_details
 	insert into cust_details values('name','email','1234567','5000')
+
+
+	create table user_log(
+	id int PRIMARY KEY,
+	operation varchar(50),
+	"date" date
+	)
+
+	--insert trigger
+Create trigger user_insert
+on cust_details
+for insert
+as insert into user_log(id,operation,date) select userid,'user inserted',GETDATE() from inserted
+
+select * from user_log
+
+--delete trigger
+Create trigger user_delete
+on cust_details
+for delete
+as insert into user_log(id,operation,date) select userid,'user inserted',GETDATE() from deleted
 
