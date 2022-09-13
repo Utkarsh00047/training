45ad32161908f44122eb7aebb5c9d50ced0d9e60
Utkarsh00047 <utkarsh.gupta@bhavnacorp.com>
2022-09-13 16:42:14 +0530

    thread

---
+++ dotnet/13-sept/threading/threading/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+
+namespace threading
+{
+    public class MyThread
+    {
+        public void Thread1()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    class Program
+            {
+                static void Main(string[] args)
+                {
+                    /*MyThread mt = new MyThread();
+                    Thread t1 = new Thread(new ThreadStart(mt.Thread1));
+                    Thread t2 = new Thread(new ThreadStart(mt.Thread1));
+                    t1.Start();
+                    t2.Start();
+                    */
+                    Console.WriteLine("Main Thread starts");
+                    Thread t1 = new Thread(method1)
+                    {
+                        Name = "Thread1"
+                    };
+                    Thread t2 = new Thread(method2)
+                    {
+                        Name = "Thread2"
+                    };
+                    Thread t3 = new Thread(method3)
+                    {
+                        Name = "Thread3"
+                    };
+
+
+                    t1.Start();
+                    t2.Start();
+                    t3.Start();
+                    Console.WriteLine("Main thread start");
+
+                }
+                private static void method1(object obj)
+                {
+                    Console.WriteLine("m1 start", Thread.CurrentThread.Name);
+                    for (int i = 1; i <= 5; i++)
+                    {
+                        Console.WriteLine("Method1: " + i);
+                    }
+                    Console.WriteLine("m1 end", Thread.CurrentThread.Name);
+                }
+                private static void method2(object obj)
+                {
+                    Console.WriteLine("m2 start", Thread.CurrentThread.Name);
+                    for (int i = 1; i <= 5; i++)
+                    {
+                        if (i == 3)
+                        {
+                            Console.WriteLine("perform thread opearion start");
+                            Thread.Sleep(5000);
+                            Console.WriteLine("thread operation compleated");
+                        }
+                    }
+                    Console.WriteLine("m2 end", Thread.CurrentThread.Name);
+                }
+                private static void method3(object obj)
+                {
+                    Console.WriteLine("m3 start", Thread.CurrentThread.Name);
+                    for (int i = 1; i <= 5; i++)
+                    {
+                        if (i == 3)
+                        {
+                            Console.WriteLine("perform thread opearion start");
+                            Thread.Sleep(5000);
+                            Console.WriteLine("thread operation compleated");
+                        }
+                    }
+                    Console.WriteLine("m3 end", Thread.CurrentThread.Name);
+                }
+            }
+        }
+    
