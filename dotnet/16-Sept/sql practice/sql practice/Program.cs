41f2d3467db8dd12c313abbd1ecab4dc8b064633
Utkarsh00047 <utkarsh.gupta@bhavnacorp.com>
2022-09-16 15:20:15 +0530

    snake

---
+++ dotnet/16-Sept/sql practice/sql practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sql_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Snake snake = new Snake();
+            while (true)
+            {
+                snake.WriteBorad();
+                snake.Input();
+                snake.Logic();
+            }
+            Console.ReadKey();
+        }
+    }
+}
