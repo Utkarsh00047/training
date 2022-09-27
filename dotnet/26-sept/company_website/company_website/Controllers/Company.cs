dcd8a52412139b9ae34ca51d3b81e75bcae14e82
Utkarsh00047 <utkarsh.gupta@bhavnacorp.com>
2022-09-27 09:18:00 +0530

    web app

---
+++ dotnet/26-sept/company_website/company_website/Controllers/Company.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace company_website.Controllers
+{
+    public class Company : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+    }
+}
